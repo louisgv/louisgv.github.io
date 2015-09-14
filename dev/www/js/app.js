d5c65227baecd76088c801cf8fc8eb56d14daeb4
Louis Vichy <lgvichy@gmail.com>
2015-09-14 15:18:58 -0700

    MODule to COMponents and from templates to MODal

--- dev/www/js/app.js
+++ dev/www/js/app.js
@@ -23,52 +23,42 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
+
+  // if none of the states below are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/');
+
   $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl',
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      menuContent: {
-        templateUrl: 'templates/search.html',
-      },
-    },
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        menuContent: {
-          templateUrl: 'templates/browse.html',
-        },
-      },
+    .state('menu', { //TODO: Change to MenuCtrl
+      url: '/menu',
+      abstract: true,
+      templateUrl: 'mod/menu.html',
+      controller: 'HomeCtrl',
     })
-    .state('app.playlists', {
-      url: '/playlists',
+    .state('home', { // TODO: Make into Home Page
+      url: '/',
       views: {
         menuContent: {
-          templateUrl: 'templates/playlists.html',
+          templateUrl: 'mod/playlists.html',
           controller: 'PlaylistsCtrl',
         },
       },
     })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      menuContent: {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl',
+    .state('jam', {
+      url: '/jam',
+      views: {
+        menuContent: {
+          templateUrl: 'com/jam/tmpl.html',
+          controller: 'PlaylistCtrl',
+        },
       },
-    },
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+    })
+    .state('hat', {
+      url: '/hat',
+      views: {
+        menuContent: {
+          templateUrl: 'com/jam/tmpl.html',
+          controller: 'PlaylistCtrl',
+        },
+      },
+    });
 });
