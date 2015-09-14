47a868f2fb1ca18a710cd0981f9935e1335e6dba
Louis Vichy <lgvichy@gmail.com>
2015-09-14 16:07:07 -0700

    Added imgold directive

---
+++ dev/www/dir/imgold.js
@@ -0,0 +1,34 @@
+app.directive('imageonload', function() {
+
+  'use strict';
+
+  return {
+    restrict: 'A',
+
+    link: function(scope, element, attrs) {
+
+      var cssClass = attrs.loadedclass;
+      var origSrc;
+      var timeout;
+
+      function reload() {
+        timeout = setTimeout(function() {
+          if (!origSrc) {
+            origSrc = element[0].src;
+          }
+
+          element[0].src = origSrc + '?' + Date.now();
+          reload();
+        }, 4500);
+      }
+
+      reload();
+
+      element.bind('load', function(e) {
+        clearTimeout(timeout);
+        element.addClass(cssClass);
+        scope.$apply(attrs.imageonload);
+      });
+    },
+  };
+});
