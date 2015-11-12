767e73a2a78389493db55970b0c250f26ab2af5c
Louis Vichy <lgvichy@gmail.com>
2015-11-12 12:51:21 -0800

    Refined the front page a lil bit

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
+$(document)
+  .ready(function() {
 
-  var g = $(".greeting"),
+    var g = $(".greeting"),
       time = new Date(),
       h = time.getHours();
-  if (h>18){
-    g.text("\tGood evening, I am")
-  }
-  else if(h>12){
-    g.html("    Good afternoon, I am");
-  }
-  else {
-    g.html("    Good morning! I am")
-  }
-})
+    if (h > 18) {
+      g.text("\tGood evening, I am")
+    } else if (h > 12) {
+      g.html("    Good afternoon, I am");
+    } else {
+      g.html("    Good morning! I am")
+    }
+
+    $('a').click(function() {
+      $('html, body').animate({
+          scrollTop: $($.attr(this, 'href'))
+          .offset()
+          .top
+      }, 900);
+      return false;
+    });
+
+  })
